d6f6f806b8098ffac519815eddef3042fd5966e3
Matias Anoniz <matiasanoniz@gmail.com>
2024-08-18 18:36:37 -0300

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, Educacion IT!' });
+    res.json({ message: 'Hello, Educacionit!' });
 });
 
 const port = process.env.PORT || 3000;
