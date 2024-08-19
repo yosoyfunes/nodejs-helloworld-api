531cfe3b90f5cc8078aaf23c5a001cc30630c0de
Matias Anoniz <matiasanoniz@gmail.com>
2024-08-19 20:23:42 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
     it('responds with hello world message in JSON format', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ message: 'Hello, Educacionit!' });
+        expect(response.body).toEqual({ message: 'Hello, Educacionit!2' });
     });
 });
